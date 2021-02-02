b9795733d604c9241b235a08a6db654063caa118
Naresh Kumar H <nareshkumarh@live.com>
2021-02-02 12:49:19 +0530

    Updated

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {TicketAPI} = require('./TicketApi');
+import { TicketAPI } from "./TicketApi.js";
 
 const ticketAPI = new TicketAPI();
 let trains = ticketAPI.search("Chennai Central","Bangalore","2021-02-02");
