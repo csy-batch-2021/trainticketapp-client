39292b95c604b24906a67ef35cd391b52e82f37c
Naresh Kumar H <nareshkumarh@live.com>
2021-02-02 11:51:30 +0530

    Updated

---
+++ Ticket.js
@@ -0,0 +1,9 @@
+class Ticket{
+    constructor(source,destination,journeyDate){
+        this.source= source;
+        this.destination = destination;
+        this.journeyDate = journeyDate;
+    }
+}
+
+exports.Ticket = Ticket;
---
+++ TicketApi.js
@@ -0,0 +1,17 @@
+class TicketAPI {
+
+    trains = [
+        { trainNo:123, from:"Chennai Central",to: "Bangalore", type:"DAILY", status:"CANCELLED"},
+        { trainNo:124, from:"Chennai Central",to: "Bangalore", type:"MON,THU"},
+        { trainNo:125, from:"Chennai Central",to: "Bangalore", type:"WED"}
+    ];
+
+    search(source,destination, journeyDate){
+        let filteredTrains =  this.trains;
+        //implement logic
+        return trains;
+    }
+}
+
+
+exports.TicketAPI = TicketAPI;
